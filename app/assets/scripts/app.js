bbf326feb513de91170f908bd2589ae19d440fcf
Steffen Harnack <s.harnack@mail.de>
2017-08-22 12:56:31 +0200

    integrated webpack

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,10 +1,3 @@
-function Person(fullName, passion) {
-	this.name = fullName,
-	this.passion = passion,
-	this.greet = function() {
-		console.log("Hi! I'm " + this.name + "! I love " + this.passion + "!");
-	}
-}
 
 var steffen = new Person("Steffen", "art");
 steffen.greet();
